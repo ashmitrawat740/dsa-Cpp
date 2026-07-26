53951855305b191bf504a24515146947e36cec70
Ashmit Rawat <ashmitrawat740@gmail.com>
2026-07-26 22:51:34 +0530

    feat: revise upon topics of array traversals

--- arraytraversal.cpp
+++ arraytraversal.cpp
@@ -31,23 +31,23 @@
 //     }
 // };
 
-Leetcode 1480
-class Solution {
-public:
-    vector<int> runningSum(vector<int>& nums) {
+// Leetcode 1480
+// class Solution {
+// public:
+//     vector<int> runningSum(vector<int>& nums) {
 
-        int n = nums.size();
+//         int n = nums.size();
 
-        vector<int> ans(n);
+//         vector<int> ans(n);
 
-        for(int i = 0; i < n; i++)
-        {
-            int sum = 0;
+//         for(int i = 0; i < n; i++)
+//         {
+//             int sum = 0;
 
-            for(int j = 0; j <= i; j++)
-            {
-                sum += nums[j];
-            }
+//             for(int j = 0; j <= i; j++)
+//             {
+//                 sum += nums[j];
+//             }
 
-            ans[i] = sum;
-        }
+//             ans[i] = sum;
+//         }
