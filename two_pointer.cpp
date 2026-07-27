95a562f9f4b03afe9523ed8e59a493bef30869ee
Ashmit Rawat <ashmitrawat740@gmail.com>
2026-07-28 00:39:16 +0530

    feat: solve two pointer questions array

--- two_pointer.cpp
+++ two_pointer.cpp
@@ -8,3 +8,46 @@ vector<int> rotateArray(vector<int> &arr, int n) {
     arr[n-1] = temp;
     return arr;
 }
+
+Rotate array by D space
+#include <bits/stdc++.h>
+#include <iostream>
+using namespace std;
+void leftRotate(int arr[], int n, int d) {
+    reverse(arr, arr+d);
+    reverse(arr+d, arr+n);
+    reverse(arr, arr+n);
+}
+int main() {
+    int n;
+    cin >> n;
+    int arr[n];
+    for(int i=0;i<n;i++) {
+        cin >> arr[i];
+    }
+    int d;
+    cin >> d;
+    leftRotate(arr,n,d);
+    for(int i=0;i<n;i++) {
+        cout << arr[i] << " ";}
+    return 0;
+}
+
+Move zeros to end 
+vector<int> moveZeros(int n, vector<int> a) {
+    int j = -1;
+    for(int i = 0; i < n; i++) {
+        if(a[i] == 0) {
+            j = i;
+            break;
+        }
+    }
+    if(j == -1) return a;
+    for(int i = j + 1; i < n; i++) {
+        if(a[i] != 0) {
+            swap(a[i], a[j]);
+            j++;
+        }
+    }
+    return a;
+}
